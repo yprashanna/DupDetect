8e0216bd51bf88769d200040f27b4b3e58900095
Prashanna Kumar Yadav <130278308+yprashanna@users.noreply.github.com>
2025-05-23 22:08:34 +0530

    add model in gdrive and attached link here

--- Streamlit_App/app.py
+++ Streamlit_App/app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import helper
 import pickle
+import gdown
+import os
 
-model = pickle.load(open('model.pkl','rb'))
+def fetch_model():
+    model_path = "model.pkl"
+    if not os.path.exists(model_path):
+        file_id = "1jYJFaoQ5LEt4TOCX6FFIq0XvAy1TDbID"
+        url = f"https://drive.google.com/uc?export=download&id={file_id}"
+        gdown.download(url, model_path, quiet=False)
+    return pickle.load(open(model_path, 'rb'))
+
+# Load model
+model = fetch_model()
 
 st.header('DupDetect')
 
@@ -10,7 +21,7 @@ q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
 
 if st.button('Find'):
-    query = helper.query_point_creator(q1,q2)
+    query = helper.query_point_creator(q1, q2)
     result = model.predict(query)[0]
 
     if result:
